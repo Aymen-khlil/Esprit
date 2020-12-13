bf451717f38442c924f3daf95491c66ae75ca5e6
Aymen-khlil <aymen-khlil@outlook.com>
2020-12-13 19:37:37 +0100

    first commit

--- src/app/create-product/create-product.component.ts
+++ src/app/create-product/create-product.component.ts
@@ -19,7 +19,7 @@ export class CreateProductComponent implements OnInit {
   newprod(form){
     
     let newproduct = {
-      id: 26,
+      id: 66,
       ProdName: form.value.name,
       category_id: form.value.categ,
       Image:'./assets/data/'+form.value.img+'.png',
